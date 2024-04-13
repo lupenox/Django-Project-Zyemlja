8ca32bbad509a18653183b46af5ff544f3f59d5a
LoganWatson1400 <lawatson@uwm.edu>
2024-04-13 13:36:52 -0500

    Added SubClasses to base Classes

--- project_app/tests/dashboardView.py
+++ project_app/tests/dashboardView.py
@@ -3,20 +3,20 @@ import unittest
 
 class TestData(unittest.TestCase):
 
-    # getDahboardData()
-    # pre
-    def test_1(self):
-        # Cant take a null
-        pass
+    class TestGetDashboardData(unittest.TestCase):
+        # pre
+        def test_1(self):
+            # Cant take a null
+            pass
 
-    def test_2(self):
-        # Must take a user in the system
-        pass
+        def test_2(self):
+            # Must take a user in the system
+            pass
 
-    # pos
-    def test_3(self):
-        # Must correctly return data
-        pass
+        # pos
+        def test_3(self):
+            # Must correctly return data
+            pass
 
 
 if __name__ == '__main__':
