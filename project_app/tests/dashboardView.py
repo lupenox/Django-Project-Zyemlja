def4403db5bcde54617aeb7a0c376878c165539d
LoganWatson1400 <lawatson@uwm.edu>
2024-04-13 13:04:14 -0500

    Formats of dashboard added base methods for instEditContact the file names seem excessive. i could use some review of them

--- project_app/tests/dashboardView.py
+++ project_app/tests/dashboardView.py
@@ -6,16 +6,16 @@ class TestData(unittest.TestCase):
     # getDahboardData()
     # pre
     def test_1(self):
-        # This test will verify that getDahboardData() cant take a null
+        # Cant take a null
         pass
 
     def test_2(self):
-        # This test will verify that getDahboardData() must have a user in the system
+        # Must take a user in the system
         pass
 
     # pos
     def test_3(self):
-        # This test will check that getDahboardData() correctly returns data
+        # Must correctly return data
         pass
 
 
