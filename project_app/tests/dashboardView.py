7b79247b82e76be940424d12ebd2ad6088921626
LoganWatson1400 <123488364+LoganWatson1400@users.noreply.github.com>
2024-04-16 12:25:01 -0500

    Revert "Added SubClasses to base Classes"
    This reverts commit c3586848efe4d04495273ec325ac74b5ebc725b0.

--- project_app/tests/dashboardView.py
+++ project_app/tests/dashboardView.py
@@ -3,20 +3,20 @@ import unittest
 
 class TestData(unittest.TestCase):
 
-    class TestGetDashboardData(unittest.TestCase):
-        # pre
-        def test_1(self):
-            # Cant take a null
-            pass
+    # getDahboardData()
+    # pre
+    def test_1(self):
+        # Cant take a null
+        pass
 
-        def test_2(self):
-            # Must take a user in the system
-            pass
+    def test_2(self):
+        # Must take a user in the system
+        pass
 
-        # pos
-        def test_3(self):
-            # Must correctly return data
-            pass
+    # pos
+    def test_3(self):
+        # Must correctly return data
+        pass
 
 
 if __name__ == '__main__':
