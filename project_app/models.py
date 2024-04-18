1246d2e388386c9ab336bdbc8515f667564801ce
loganlapierre <lapierr3@uwm.edu>
2024-04-18 10:37:12 -0700

    removed a conflicting class

--- project_app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-#needs to be edited later to reflect our program for database purposes
-class Patient:
-    first_name = models.charField(max_length=50)
-    age = models.IntegerField
-    Physician = models.ForeignKey(Physician, on_delete=models, null=true)
