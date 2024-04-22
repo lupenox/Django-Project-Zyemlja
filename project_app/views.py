9ef1d550f70f324a6aa142d8eb4a73b7654e4267
Arianne <ingbret2@uwm.edu>
2024-04-22 12:06:02 -0500

    views skeleton redone

--- project_app/views.py
+++ project_app/views.py
@@ -1,29 +1,39 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
-def userManagementView(request):
-    return None
 
+class dashboardView(View):
+    def get(self, request):
 
-def dashboardView(request):
-    return None
+    def post(self, request):
 
+class loginView(View):
+    def get(self, request):
 
-def loginView(request):
-    return None
+    def post(self, request):
 
+class dashboardView(View):
+    def get(self, request):
 
-def courseManagementView(request):
-    return None
+    def post(self, request):
 
+class userManageView(View):
+    def get(self, request):
 
-def instructorCoursesView(request):
-    return None
+    def post(self, request):
 
+class courseManageView(View):
+    def get(self, request):
 
-def assignmentsTAView(request):
-    return None
+    def post(self, request):
 
+class contactInfoView(View):
+    def get(self, request):
 
-def contactInfoView(request):
-    return None
+    def post(self, request):
+
+class courseAssignmentView(View):
+    def get(self, request):
+
+    def post(self, request):
