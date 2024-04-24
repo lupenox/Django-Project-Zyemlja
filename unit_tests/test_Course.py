ce95f09b84d2b512c5abd1735e5defa3fa4ba8e8
LoganWatson1400 <lawatson@uwm.edu>
2024-04-23 20:31:22 -0500

    test_course

--- unit_tests/test_Course.py
+++ unit_tests/test_Course.py
@@ -1,35 +1,20 @@
-import unittest, course
-from unittest.mock import Mock
+from django.test import TestCase
+from classes.course import CourseClass
 
 
-class MyTestCase(unittest.TestCase):
+class TestCourseClass(TestCase):
+    class TestAddCourseName(TestCase):
+        def setUp(self):
+            self.course = CourseClass()
 
-    def setUp(self):
-        mock = CourseClass(name="CS361", instructors="Rock", ta=["Apoorv", "Tarun"],
-                           sections=["801", "802", "803", "804"])
+        def test_base(self):
+            self.course.set_course_name("test")
+            self.assertEqual(self.course.course_name, "test")
 
-    def test_addTA(self):
-        self.assertTrue(mock.addTA("Lizze"))
-        self.assertIn("Lizzie", mock.ta)
-        self.assertFalse(mock.addTA(1))
+        def test_no_arg(self):
+            with self.assertRaises(TypeError):
+                self.course.set_course_name()
 
-    def test_removeTA(self):
-        self.assertTrue(mock.removeTA("Apoorv"))
-        self.assertNotIn("Apoorv", mock.ta)
-        self.assertFalse(mock.removeTA("Lizzie"))
-
-    def test_addSection(self):
-        self.assertTrue(mock.addSection("805"))
-        self.assertIn("805", mock.sections)
-        self.assertFalse(mock.addSection(2))
-
-    def test_removeSection(self):
-        self.assertTrue(mock.removeSection("801"))
-        self.assertNotIn("801", mock.sections)
-        self.assertFalse(mock.removeSection("609"))
-
-    def test_getTAs(self):
-        self.assertEqual(mock.getTAs(), ["Apoorv", "Tarun"])
-
-    def test_getSections(self):
-        self.assertEqual(mock.getSections(), ["801", "802", "803", "804"])
+        def test_invalid_type(self):
+            with self.assertRaises(TypeError):
+                self.course.set_course_name(1)
